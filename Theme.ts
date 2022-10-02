800d103ea78aacc8247ee43c3b701a80efc4132c
JorgeRojo <jorge.rojodiseno@gmail.com>
2022-10-02 16:49:25 +0000

    Deploying to gh-pages from @ marythe-ux/red-and-king-components-tokens@eee13602b590c20d0cd5636be2c6dc8d07cd4ab1 🚀

--- Theme.ts
+++
@@ -1,9 +0,0 @@
-import { create } from '@storybook/theming';
-import logo from './public/topdone.svg';
-
-export default create({
-  base: 'light',
-  brandTitle: 'Red&King components',
-  brandUrl: 'https://marythe-ux.github.io/red-and-king-components/storybook',
-  brandImage: logo,
-});
